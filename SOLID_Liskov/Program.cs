43f7e540cc0cf706cca46d9dcdd1f0ff57c3f305
Jadran Grba <JadranG@ebe-inc.com>
2019-05-23 10:56:11 +0200

    new code patterns: - solid Liskov - solid srp

---
+++ SOLID_Liskov/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace SOLID_Liskov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 5, 7, 9, 8, 1, 6, 4 };
+
+            //we are keeping our functionality intact and still having our subclasses act as a substitute to a base class.
+
+            Calculator sum = new SumCalculator(numbers);
+            Console.WriteLine($"The sum of all the numbers: {sum.Calculate()}");
+
+            Console.WriteLine();
+
+            Calculator evenSum = new EvenNumbersSumCalculator(numbers);
+            Console.WriteLine($"The sum of all the even numbers: {evenSum.Calculate()}");
+        }
+    }
+
+    public abstract class Calculator
+    {
+        protected readonly int[] _numbers;
+
+        public Calculator(int[] numbers)
+        {
+            _numbers = numbers;
+        }
+
+        public abstract int Calculate();
+    }
+
+    public class SumCalculator : Calculator
+    {
+        public SumCalculator(int[] numbers)
+        : base(numbers)
+        {
+        }
+
+        public override int Calculate() => _numbers.Sum();
+    }
+
+    public class EvenNumbersSumCalculator : Calculator
+    {
+        public EvenNumbersSumCalculator(int[] numbers)
+            : base(numbers)
+        {
+        }
+
+        public override int Calculate() => _numbers.Where(x => x % 2 == 0).Sum();
+    }
+
+}
---
+++ SOLID_SRP/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+/// <summary>
+/// Single Responsibility Principle
+/// </summary>
+namespace SOLID_SRP
+{
+    /// <summary>
+    /// Single Responsibility Principle
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var report = new WorkReport();
+            report.AddEntry(new WorkReportEntry { ProjectCode = "123Ds", ProjectName = "Project1", SpentHours = 5 });
+            report.AddEntry(new WorkReportEntry { ProjectCode = "987Fc", ProjectName = "Project2", SpentHours = 3 });
+
+            var scheduler = new Scheduler();
+            scheduler.AddEntry(new ScheduleTask { TaskId = 1, Content = "Do something now.", ExecuteOn = DateTime.Now.AddDays(5) });
+            scheduler.AddEntry(new ScheduleTask { TaskId = 2, Content = "Don't forget to...", ExecuteOn = DateTime.Now.AddDays(2) });
+
+            Console.WriteLine(report.ToString());
+            Console.WriteLine(scheduler.ToString());
+
+            var saver = new FileSaver();
+            saver.SaveToFile(@"Reports", "WorkReport.txt", report);
+            saver.SaveToFile(@"Schedulers", "Schedule.txt", scheduler);
+        }
+    }
+
+    public class WorkReportEntry
+    {
+        public string ProjectCode { get; set; }
+        public string ProjectName { get; set; }
+        public int SpentHours { get; set; }
+    }
+
+    public class ScheduleTask
+    {
+        public int TaskId { get; set; }
+        public string Content { get; set; }
+        public DateTime ExecuteOn { get; set; }
+    }
+
+    public interface IEntryManager<T>
+    {
+        void AddEntry(T entry);
+        void RemoveEntryAt(int index);
+    }
+
+    public class FileSaver
+    {
+        public void SaveToFile<T>(string directoryPath, string fileName, IEntryManager<T> workReport)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            File.WriteAllText(Path.Combine(directoryPath, fileName), workReport.ToString());
+        }
+    }
+
+
+    public class WorkReport : IEntryManager<WorkReportEntry>
+    {
+        private readonly List<WorkReportEntry> _entries;
+
+        public WorkReport()
+        {
+            _entries = new List<WorkReportEntry>();
+        }
+
+        public void AddEntry(WorkReportEntry entry) => _entries.Add(entry);
+
+        public void RemoveEntryAt(int index) => _entries.RemoveAt(index);
+
+        /*MOVED TO ANOTHER CLASS*/
+        //public void SaveToFile(string directoryPath, string fileName)
+        //{
+        //    if (!Directory.Exists(directoryPath))
+        //    {
+        //        Directory.CreateDirectory(directoryPath);
+        //    }
+
+        //    File.WriteAllText(Path.Combine(directoryPath, fileName), ToString());
+        //}
+
+        public override string ToString() =>
+            string.Join(Environment.NewLine, _entries.Select(x => $"Code: {x.ProjectCode}, Name: {x.ProjectName}, Hours: {x.SpentHours}"));
+    }
+
+    public class Scheduler : IEntryManager<ScheduleTask>
+    {
+        private readonly List<ScheduleTask> _scheduleTasks;
+
+        public Scheduler()
+        {
+            _scheduleTasks = new List<ScheduleTask>();
+        }
+
+        public void AddEntry(ScheduleTask entry) => _scheduleTasks.Add(entry);
+
+        public void RemoveEntryAt(int index) => _scheduleTasks.RemoveAt(index);
+
+        public override string ToString() =>
+            string.Join(Environment.NewLine, _scheduleTasks.Select(x => $"Task with id: {x.TaskId} with content: {x.Content} is going to be executed on: {x.ExecuteOn}"));
+    }
+}
