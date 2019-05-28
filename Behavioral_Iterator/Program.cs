5dcd6f28bc007686cbf01d20f4a030e0d7ba04e6
Jadran Grba <JadranG@ebe-inc.com>
2019-05-28 14:55:02 +0200

    new patterns added

---
+++ Behavioral_Iterator/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections;
+
+/// <summary>
+/// Provide a way to access the elements of an aggregate object sequentially without exposing its underlying representation. 
+/// </summary>
+namespace Behavioral_Iterator
+{
+    class Program
+    {
+        /// <summary>
+        /// A collection item
+        /// </summary>
+        class Item
+        {
+            // Constructor
+            public Item(string name)
+            {
+                Name = name;
+            }
+            
+            // Gets name
+            public string Name { get; }
+        }
+
+
+        /// <summary>
+        ///  interface for creating an Iterator object
+        /// </summary>
+        interface IAbstractCollection
+        {
+            Iterator CreateIterator();
+        }
+
+
+        /// <summary>
+        /// implements the Iterator creation interface
+        /// </summary>
+        class Collection : IAbstractCollection
+        {
+            private ArrayList _items = new ArrayList();
+
+            public Iterator CreateIterator()
+            {
+                return new Iterator(this);
+            }
+
+            // Gets item count
+            public int Count
+            {
+                get { return _items.Count; }
+            }
+            
+            // Indexer
+            public object this[int index]
+            {
+                get { return _items[index]; }
+                set { _items.Add(value); }
+            }
+        }
+
+        /// <summary>
+        /// interface for accessing elements.
+        /// </summary>
+        interface IAbstractIterator
+        {
+            Item First();
+            Item Next();
+            bool IsDone { get; }
+            Item CurrentItem { get; }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        class Iterator : IAbstractIterator
+        {
+            private Collection _collection;
+            private int _current = 0;
+
+            // Constructor
+            public Iterator(Collection collection)
+            {
+                _collection = collection;
+            }
+            
+            // Gets first item
+            public Item First()
+            {
+                _current = 0;
+                return _collection[_current] as Item;
+            }
+            
+            // Gets next item
+            public Item Next()
+            {
+                _current += Step;
+                if (!IsDone)
+                    return _collection[_current] as Item;
+                else
+                    return null;
+            }
+            
+            // Gets or sets stepsize
+            public int Step { get; set; } = 1;
+
+            // Gets current iterator item
+            public Item CurrentItem
+            {
+                get { return _collection[_current] as Item; }
+            }
+            
+            // Gets whether iteration is complete
+            public bool IsDone
+            {
+                get { return _current >= _collection.Count; }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Build a collection
+            Collection collection = new Collection();
+            collection[0] = new Item("Item 0");
+            collection[1] = new Item("Item 1");
+            collection[2] = new Item("Item 2");
+            collection[3] = new Item("Item 3");
+            collection[4] = new Item("Item 4");
+            collection[5] = new Item("Item 5");
+            collection[6] = new Item("Item 6");
+            collection[7] = new Item("Item 7");
+            collection[8] = new Item("Item 8");
+            
+            // Create iterator
+            Iterator iterator = collection.CreateIterator();
+
+            // Skip every other item
+            iterator.Step = 2;
+            
+            Console.WriteLine("Iterating over collection:");
+
+            for (Item item = iterator.First();
+                !iterator.IsDone; item = iterator.Next())
+            {
+                Console.WriteLine(item.Name);
+            }
+            
+            // Wait for user
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Structural_Facade/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+
+/// <summary>
+/// Structural Facade code pattern:
+/// A single class that represents an entire subsystem
+/// Provide a unified interface to a set of interfaces in a subsystem
+/// A Facade shields the user from the complex details of the system and provides them with a simplified 
+/// view of it which is easy to use. It also decouples the code that uses the system from the details 
+/// of the subsystems, making it easier to modify the system later.
+/// </summary>
+namespace Structural_Facade
+{
+    /// <summary>
+    /// The 'Subsystem ClassA' class
+    /// </summary>
+    class Bank
+    {
+        public bool HasSufficientSavings(Customer c, int amount)
+        {
+            Console.WriteLine("Check bank for " + c.Name);
+            return true;
+        }
+    }
+
+    
+    /// <summary>
+    /// The 'Subsystem ClassB' class
+    /// </summary>
+    class Credit
+    {
+        public bool HasGoodCredit(Customer c)
+        {
+            Console.WriteLine("Check credit for " + c.Name);
+            return true;
+        }
+    }
+    
+
+    /// <summary>
+    /// The 'Subsystem ClassC' class
+    /// </summary>
+    class Loan
+    {
+        public bool HasNoBadLoans(Customer c)
+        {
+            Console.WriteLine("Check loans for " + c.Name);
+            return true;
+        }
+    }
+    
+
+    /// <summary>
+    /// Customer class
+    /// </summary>
+    class Customer
+    {
+        // Constructor
+        public Customer(string name)
+        {
+            Name = name;
+        }
+        
+        // Gets the name
+        public string Name { get; }
+
+    }
+    
+    /// <summary>
+    /// The 'Facade' class
+    /// </summary>
+    class Mortgage
+    {
+        private Bank _bank = new Bank();
+        private Loan _loan = new Loan();
+        private Credit _credit = new Credit();
+        
+        public bool IsEligible(Customer cust, int amount)
+
+        {
+            Console.WriteLine("{0} applies for {1:C} loan\n", cust.Name, amount);
+            bool eligible = true;
+
+            // Check creditworthyness of applicant
+            if (!_bank.HasSufficientSavings(cust, amount))
+            {
+                eligible = false;
+            }
+            else if (!_loan.HasNoBadLoans(cust))
+            {
+                eligible = false;
+            }
+            else if (!_credit.HasGoodCredit(cust))
+            {
+                eligible = false;
+            }
+
+            return eligible;
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {      
+                // Facade
+                Mortgage mortgage = new Mortgage();
+                
+                // Evaluate mortgage eligibility for customer
+                Customer customer = new Customer("Ann McKinsey");
+                bool eligible = mortgage.IsEligible(customer, 125000);
+                
+                Console.WriteLine("\n" + customer.Name +
+                    " has been " + (eligible ? "Approved" : "Rejected"));
+
+                // Wait for user
+                Console.ReadKey();
+            }
+        }
+    }
+}
