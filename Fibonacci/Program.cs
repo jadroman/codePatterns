45bc56a5696b8de1d8eedabdcf0ace0c9d6b09db
jadro <jadra@DESKTOP-K5KEPAI>
2020-01-18 12:20:53 +0100

    novi primjeri

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        /// <summary>
+        /// Next number is the sum of previous two numbers for example 0, 1, 1, 2, 3, 5, 8, 13, 21 etc
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int num1 = 0, num2 = 1, num3, numOfElements, counter;
+            Console.WriteLine("How much elements?");
+            numOfElements = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"{num1}, {num2}");
+
+            for (counter = 2; counter < numOfElements; counter++)
+            {
+                num3 = num1 + num2;
+                Console.Write($", {num3}");
+                num1 = num2;
+                num2 = num3;
+            }
+        }
+    }
+}
---
+++ FluentInterface/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace FluentInterface
+{
+    class Program
+    {
+        public class Customer
+        {
+            public string FullName { get; set; }
+            public DateTime Dob { get; set; }
+            public string Address { get; set; }
+
+        }
+
+        public class CustomerFluent
+        {
+            public string Adress { get { return obj.Address; } }
+
+            private Customer obj = new Customer();
+            public CustomerFluent NameOfCustomer(string Name)
+            {
+                obj.FullName = Name;
+                return this;
+            }
+            public CustomerFluent Bornon(string Dob)
+            {
+                obj.Dob = Convert.ToDateTime(Dob);
+                return this;
+            }
+
+            public void StaysAt(string Address) => obj.Address = Address;
+        }
+
+        static void Main(string[] args)
+        {
+            CustomerFluent customer = new CustomerFluent();
+
+            customer.NameOfCustomer("Shiv")
+                   .Bornon("12/3/1075")
+                   .StaysAt("Mumbai");
+
+            Console.WriteLine(customer.Adress);
+        }
+    }
+}
