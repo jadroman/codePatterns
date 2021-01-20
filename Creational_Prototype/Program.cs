e7da12d2e62b068f4b354120df35ea02dad4f59d
Jadran Grba <jadran.grba@euris.it>
2021-01-20 12:30:53 +0100

    Prototype pattern added

---
+++ Creational_Prototype/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Creational_Prototype
+{
+    class Program
+    {
+        public abstract class Prototype
+        {
+            // normal implementation
+
+            public abstract Prototype Clone();
+        }
+
+        public class ConcretePrototype1 : Prototype
+        {
+            public override Prototype Clone()
+            {
+                return (Prototype)this.MemberwiseClone(); // Clones the concrete class.
+            }
+
+            public string SomeMethod => "ConcretePrototype1 method";
+            
+        }
+
+        public class ConcretePrototype2 : Prototype
+        {
+            public override Prototype Clone()
+            {
+                return (Prototype)this.MemberwiseClone(); // Clones the concrete class.
+            }
+
+
+            public string SomeMethod => "ConcretePrototype2 method";
+        }
+
+        static void Main(string[] args)
+        {
+            ConcretePrototype1 cp1 = new ConcretePrototype1();
+            ConcretePrototype1 c1_1 = (ConcretePrototype1)cp1.Clone();
+
+            Console.WriteLine(c1_1.SomeMethod);
+        }
+    }
+}
